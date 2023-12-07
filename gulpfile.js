d861e66cab0aff121076bd851a33f9017e29d196
Tofan <tofan.rukmana@bankina.id>
2023-12-07 11:00:03 +0700

    update set index

--- gulpfile.js
+++ gulpfile.js
@@ -100,14 +100,14 @@ function html() {
     .pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
     .pipe($.if(/\.css$/, $.postcss([cssnano({safe: true, autoprefixer: false})])))
     .pipe($.if(/\.html$/, $.htmlmin({
-      collapseWhitespace: true,
-      minifyCSS: true,
-      minifyJS: {compress: {drop_console: true}},
-      processConditionalComments: true,
-      removeComments: true,
-      removeEmptyAttributes: true,
-      removeScriptTypeAttributes: true,
-      removeStyleLinkTypeAttributes: true
+      // collapseWhitespace: true,
+      // minifyCSS: true,
+      // minifyJS: {compress: {drop_console: true}},
+      // processConditionalComments: true,
+      // removeComments: true,
+      // removeEmptyAttributes: true,
+      // removeScriptTypeAttributes: true,
+      // removeStyleLinkTypeAttributes: true
     })))
     .pipe(dest('dist'));
 }
